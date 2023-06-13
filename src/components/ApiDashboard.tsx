1736c1cdb8cdde6b24c7a6a1760123a72c382e5b
Gabriel <gabrielgrazeffi123@gmail.com>
2023-06-13 15:10:00 -0300

    finish the dashboard

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -7,6 +7,8 @@ import { formatDistance } from "date-fns";
 import LargeHeading from "./ui/LargeHeading";
 import Paragraph from "./ui/Paragrahph";
 import { Input } from "./ui/Input";
+import Table from "./ui/Table";
+import ApiKeyOptions from "./ApiKeyOptions";
 
 const ApiDashboard = async () => {
   const user = await getServerSession(authOptions);
@@ -39,13 +41,14 @@ const ApiDashboard = async () => {
       <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center">
         <Paragraph>Your API key:</Paragraph>
         <Input className="w-fit truncate" readOnly value={activeApiKey.key} />
+        <ApiKeyOptions apiKeyKey={activeApiKey.key} />
       </div>
 
       <Paragraph className="text-center md:text-left mt-4 -mb-4">
         Your API history:
       </Paragraph>
 
-      <Table />
+      <Table userRequests={serializableRequests} />
     </div>
   );
 };
